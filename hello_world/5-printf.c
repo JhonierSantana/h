b45b2b7f2b5201e4f8eea765cd525f9e85b7bd1f
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-07 12:05:56 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,20 +1,19 @@
 /*
  * File: 5-printf.c
  * Auth: Jhonier Santana
-*/
+ */
 
-#incluide <stdio.h>
+#include <stdio.h>
 
 /**
- * main - Prints ""with proper grammar, but the outcome is a piece of art," followed by a new line.
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	prinf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
-
