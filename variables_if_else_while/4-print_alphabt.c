abbbc460a01c36f954bff96ce265ceabc5f37f26
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-10 11:29:27 -0700

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, and then in uppercase.
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
  */
-int main(void)
+int main(void)i
 {
 	char letter;
 	char characters = ("q,e";)
