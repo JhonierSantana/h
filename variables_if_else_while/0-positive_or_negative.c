bb13e1a599e24404b20f5cc01641d3fcc4a5029b
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-10 08:05:25 -0700

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <time.h>
- 
+/* headers goes there */ 
+
+/* function main goes there */
 int main(void)
 {
    int n;
 
    srand(time(0));
    n = rand() - RAND_MAX / 2;
-
+   /* Code goes there */
    return (0);
 }
