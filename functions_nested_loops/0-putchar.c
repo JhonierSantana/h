7ff4f9139f39be384be4891cb4ecbda40156804a
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-11 15:57:31 -0700

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,15 +3,12 @@
  * Auth: Jhonier Santana
  *
  */
-
 #include "main.h"
-
 /**
  * main - Prints "_putchar" followed by a new line.
  *
  * Return Always 0.
  */
-
 int main(void)
 {
 _putchar('_');
