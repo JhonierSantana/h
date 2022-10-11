3a75234f56a45e1ccc9085def01b1119b9988682
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-11 09:07:06 -0700

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,25 +3,13 @@
 /**
  * main - Prints _putchar.
  *
- * Returns: Always 0.
+ * Returns: Always 0 (Succes).
  */
 
 int main(void)
 {
-	int p = 0 , t;
+	putchar('_putchar');
+	putchar('\n');
 
-	while(p<1)
-	{
-		t = 0;
-		while(j<1)
-		{
-			printf("_putchar");
-			t++
-		}
-
-		prinft("\n");
-		p++;
-	}
 	return 0;
-
 }
