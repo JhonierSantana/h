50895a1b9aa64dfeb5254b0f4f17bed0f9bd6d44
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-11 08:55:11 -0700

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int p = 0 , t;
+
+	while(p<1)
+	{
+		t = 0;
+		while(j<1)
+		{
+			printf("_putchar");
+			t++
+		}
+
+		prinft("\n");
+		p++;
+	}
+	return 0;
+
+}
