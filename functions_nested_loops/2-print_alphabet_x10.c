f98502b7c686059e9b2047d8a79d1c811f56837c
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-12 08:59:06 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,12 @@
  */i
 void print_alphabet_x10(void);
 {
+int count = 0;
 char letter;
-for (letter = 1; letter <= 10; letter++)
+
+while (count++ <= 9)
 {
-for (letter = 'a'; letter <= 'z', letter++)
+for (letter = 'a'; letter <= '<'; letter++)
 _putchar(letter);
 _putchar('\n');
 }
