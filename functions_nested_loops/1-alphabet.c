483f7a28a81c2855f9492253ff9a5bbf7bfcc7b4
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-12 08:09:13 -0700

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
  *
  * Auth: Jhonier Santana
  */
-#include<stdio.h>
+#include "main.h"
 /**
  * main - prints the alphabet, in lowercase.
  *
@@ -12,8 +12,7 @@
 void print_alphabet(void);
 {
 	char letter;
-	for(letter = 'a', letter <= 'z'; letter++)
-		_putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
 	_putchar('\n');
-	return (0);
 }
