cd03e8bb1bd85f8672364055fea4cd13c13d4795
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-12 12:40:31 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+/*
+ * File: 6-abs.c
+ * Auth: Jhonier Santana
+ */
+#include "main.h"
+#include<stdlib.h>
+/**
+ * _abs - prints absolute value of an integer
+ * @int
+ */
+
+int _abs(int)
+{
+	int a;
+	printf("value absolute(value absolute):";
+	scanf("%d",&a);
+}
