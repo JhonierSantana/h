0b5f1a135c85629dee18b3072951c4c0e8a6cc47
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-13 05:32:31 -0700

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -20,9 +20,9 @@ for (minute = 0; minute <= 59; minute++)
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
 _putchar(':');
-_puthcar((minute / 10) + '0');
+_putchar((minute / 10) + '0');
 _putchar((minute % 10) + '0');
-_puthcar('\n');
+_putchar('\n');
 }
 }
 }
