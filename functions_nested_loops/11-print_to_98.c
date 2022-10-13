4f554d191a84b71804e6568741f733dbfc86d55b
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-13 06:48:35 -0700

    11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+/*
+ * File: 11-print_to_98.c
+ * Auth: Jhonier Santana
+ */
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98.
+ * @n: The integer to be computer.
+ *
+ * Return Always 0.
+ */
+
+void print_to_98(int n);
+{
+	for(n = '0'; <= '98'; n++)
+		_putchar('n');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('\n');
+
+		return (0);
+}
