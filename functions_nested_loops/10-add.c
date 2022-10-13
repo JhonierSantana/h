698dbc0322429abdfcb5338edf2096e183d2bfd8
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-13 06:36:19 -0700

    10-add.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -8,10 +8,10 @@
  * @num1: The first integer to be added.
  * @num2: The second integer to be added.
  *
- * Return: adds the result.
+ * Return: adds function add.
  */
 
 int add(int num1, int num2)
 {
-	return (num1 +num2);
+	return (num1 + num2);
 }
