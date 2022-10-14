b3b4259ca05532424c09829ada38f5aa997517d4
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-14 08:25:29 -0700

    2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -3,13 +3,13 @@
  * Auth: Jhonier Santana
  */
 #include "main.h"
+
 /**
- * mul - checks for checks for a digit.
- * @a: a - variable
+ * mul -  checks for checks for a digit (0 through 9).
+ * @a: a -  Variable
  * @b: b - variable
  * Return: Always 0.
  */
-
 int mul(int a, int b)
 {
 	int mul;
