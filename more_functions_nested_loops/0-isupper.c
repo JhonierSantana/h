edf43c6676630964a9c4e0b39797f050c52c85d2
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-14 07:38:19 -0700

    agrego

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+/*
+ * File: 0-isupper.c
+ * Auth: Jhonier Santana
+ */
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - checks for uppercase character.
+ * @c: The name variable.
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+int _isupper(int c)
+{
+
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
+}
