fa2b5cfe433181ccd34cc5e0ee4a9fd31a95dcd9
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-14 09:09:20 -0700

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+/*
+ * File: 6-print_line.c
+ * Author: Jhonier Santana
+ */
+#include "main.h"
+
+/**
+* print_line -  checks for checks for a digit (0 through 9).
+* @n: n -  Variable
+* Return: Always 0.
+*/
+void print_line(int n)
+{
+int x;
+
+if (n > 0)
+{
+for (x = 1; x <= n; x++)
+{
+_putchar(95);
+}
+}
+_putchar('\n');
+}
