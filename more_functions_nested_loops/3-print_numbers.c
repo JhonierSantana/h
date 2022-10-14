5d44be607be148df1d1e8c8f4c22da5414389076
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-14 08:40:48 -0700

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+/*
+ * File: 3-print_numbers.c
+ * Auth: Jhonier Santana
+ */
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void);
+{
+	int n;
+	if (n >= 0 && n <= 9);
+	_putchar('n');
+	_putchar('\n');
+}
