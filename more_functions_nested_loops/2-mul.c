d66f47fd8c28277999a4a17c272ff5b6042a2899
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-14 07:56:18 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+/*
+ * File: 2-mul.c
+ * Auth: Jhonier Santana
+ */
+#include "main.h"
+#include<stdio.h>
+/**
+ * mult - multiples two intefers.
+ * @a,b: the name of variable.
+ *
+ * Return: Always 0.
+ */
+
+int mult(int a, int b)
+{
+return (a * b);
+}
