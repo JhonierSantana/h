2d03f4d647a7c19cfe619420d8c1915c5e4817fe
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-14 09:00:06 -0700

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+/*
+ * File: 5-more_numbers.c
+ * Auth: Jhonier Santana
+ */
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers.
+ *
+ * Return always.
+ */
+
+void more_numbers(void)
+{
+int n, b;
+for (n = '48'; n = '57'; n++)
+	if (b = 0; b = '14'; b++)
+	{
+		_putchar(n);
+		_putchar(b);
+	}
+		_putchar('\n');
+}
