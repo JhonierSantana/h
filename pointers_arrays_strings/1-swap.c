2c8bb8c034becae8ff1743808b0ad022d125a50d
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-17 12:40:49 -0700

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+/*
+ * File: 1-swap.c
+ * Auth: Jhonier Santana
+ */
+
+#include "main.h"
+
+/**
+ * swap_int - that swaps the values of two integers.
+ *
+ * @a: pointer
+ * @b: pointer
+ */
+
+void swap_int(int *a, int *b)
+{
+ printf("\n *a is %d", *a);
+ printf("\n +b is %d", *b);
+}
