8be0e95c732015e210902a2924ee0ea13261b7f6
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-17 13:19:24 -0700

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+/*
+ * File: 3-puts.c
+ * Auth: Jhonier Santana
+ */
+
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line.
+ *
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", *str);
+}
