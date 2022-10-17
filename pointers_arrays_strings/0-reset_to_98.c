9675c375ccc9cd18e9a4b609ad04d8f8ebeed615
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-17 12:23:35 -0700

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Jhonier Santana
+ */
+#include "main.h"
+#include<stdio.h>
+/**
+ * reset_to_98 - that takes a pointer to an int as parameter and updates the value.
+ *
+ * @n: The pointer to ant int
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
