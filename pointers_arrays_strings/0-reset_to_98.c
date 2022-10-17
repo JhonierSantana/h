5a9ff7dc675fdf06da4aa5e43fbc141ed618cee5
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-17 12:29:27 -0700

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,12 @@
  * Auth: Jhonier Santana
  */
 #include "main.h"
- * reset_to_98 - that takes a pointer 
- * @n: The pointer to ant int
- */
 
+/**
+ * reset_to_98 - that takes a pointer 
+ *
+ * @n: The pointer to ant int.
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
