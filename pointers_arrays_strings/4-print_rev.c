5aea1ac1e5e35494fa6dd303d3f053d3f5ac4c47
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-18 07:39:16 -0700

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+/*
+ * File: 4-print_rev.c
+ * Auth: Jhonier Santana
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ *
+ * @s: The pointer.
+ */
+
+void print_rev(char *s)
+{
+	int length = -1
+		for (int length = -1; *s >=0; *s++);
+
+}
