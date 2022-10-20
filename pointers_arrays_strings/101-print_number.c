c7aa06d7ffb8bc4dd1f704cca41e3e35cf040076
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-20 08:57:46 -0700

    101-print_number.c

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,28 +1,21 @@
-/*
- * File: 101-print_number.c
- * Auth: Jhonier Santana
- */
-
 #include "main.h"
-
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * print_number - print an integer
+ * @n: integer to print
  */
 void print_number(int n)
 {
-	unsigned int num = n;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		if (n < -9)
+			print_number(n / -10);
+		_putchar('0' - n % 10);
 	}
-
-	if ((num / 10) > 0)
+	else
 	{
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+		if (n > 9)
+			print_number(n / 10);
+		_putchar(n % 10 + '0');
 	}
 }
