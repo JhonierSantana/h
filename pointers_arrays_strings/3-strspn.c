37168f07d5330343abfcf09e0ec0f6b298f45947
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-21 09:08:27 -0700

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,6 @@
 /*
- * Auth: Jhonier Santana
- */
+* Author: Jhonier Santana
+*/
 #include "main.h"
 
 /**
@@ -13,7 +13,7 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
-	
+
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
