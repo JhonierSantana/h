06a7a249497cb67a27acbdc75618fb2347c83285
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-21 08:56:52 -0700

    2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+/*
+ * Auth: Jhonier Santana
+ */
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to check
+ * @c: character to check for
+ *
+ * Return: pointer to spot in s with c or null
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*s(s + i) == c)
+			return (s + i);
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	return (0);
+}
