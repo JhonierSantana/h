2eeda93ff12c5bd20aee176ef36ad4750a9293c2
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-24 08:32:47 -0700

    1-print_rev_recursion.c

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+/*
+ * Auth: Jhonier Santana
+ */
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of string
+ * @s: The string to be measured
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+int len = 0;
+
+if(*s)
+{
+len++;
+lent += _strlen_recursion(s + 1);
+}
+return (len);
+}
