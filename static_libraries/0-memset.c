844133e2f6e32579b718ad7f31f310b7c44b4643
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-26 13:12:32 -0700

    static

---
+++ static_libraries/0-memset.c
@@ -0,0 +1,23 @@
+/*
+ * Auth: Jhonier Santana
+ */
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: meomory space to be filled
+ * @b: byte to fill with
+ * @n: number of spaces to fill
+ *
+ * Return: pointer to memomry area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
