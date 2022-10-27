ce4644e057e552db29e5e9de20dd4b19df7e0a67
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-27 10:37:23 -0700

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -8,9 +8,8 @@
  * @argv: argument vector
  * Return: 0.
  */
-
-int main(int argc,char *argv[])
-{		
+int main(int argc, char *argv[])
+{
 	printf("%s\n", argv[argc - 1]);
-	return(0);
+	return (0);
 }
