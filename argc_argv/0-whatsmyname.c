4eaaa1833f7d0afa72afdf67b86810b523b22b24
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-27 10:13:11 -0700

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,9 +2,8 @@
  * Auth: Jhonier Santana
  */
 #include <stdio.h>
-
 /**
- * whatsmyname - prints its name, followed by a new line
+ * main - prints its name, followed by a new line
  * @argc: argument count
  * @argv: argument vector
  * Return: 0.
@@ -12,6 +11,6 @@
 
 int main (int argc, char *argv[])
 {		
-printf("%s\n", argv[argc - 1]);
-return (0);
+	printf("%s\n", argv[argc - 1]);
+	return (0);
 }
