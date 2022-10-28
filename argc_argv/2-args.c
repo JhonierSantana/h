cf0f35a23c88fdd37a8de18482a9b298c8ce1174
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-28 08:02:26 -0700

    2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -11,12 +11,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	for(i = 0; i < argc; i++)
-	{
-		argc = argc;
-
-		printf("%s\n", i, argv[i]);
-	}
+	argc = argc;
+	printf("%s\n", argv[ - 1]);
 		return 0;
 }
