bf35edb7d55104e4773d693977630548de0cad26
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-28 07:51:06 -0700

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+/*
+ * Auth: Jhonier Santana
+ */
+#include<stdio.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: Argument
+ * @argv: Argument
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for(i = 0; i < argc; i++){
+		printf("%d, %s\n", i, argv[i]);
+		return 0;
+	}
+}
