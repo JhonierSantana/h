6381a32dba101f36504d871b563475895092f9f8
JhonierSantana <Jhonier_2504@hotmail.com>
2022-10-28 08:12:24 -0700

    2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -11,10 +11,10 @@
  */
 int main(int argc, char *argv[])
 {
-	int counter;
-	for(counter=0; counter<argc; counter++)
+	int i;
+	for(i = 0; i < argc; i++)
 	{
-		printf("argv[%2d]: %s\n",counter,argv[counter]);
+		printf("%s\n", argv[i]);
 	}
 	return 0;
 }
