c6bb543ee24502ee12c549c316bf76652af4d7f5
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-01 08:28:34 -0700

    0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+/*
+ * Auth: Jhonier Santana
+ */
+#include "main.h"
+#include "stdlib.h"
+/**
+ * create_array - creates an array of chars
+ * @size - array size
+ * @c: array
+ *
+ * Return: 0;
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *a;
+
+	if(size == 0)
+		return: NULL;
+
+	a = malloc(size * sizeof(char));
+
+	if(a = NULL)
+		return: NULL;
+	for(i = 0; i < size; i++)
+		a[i] = c;
+
+	return (a);
+}
