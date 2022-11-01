156b2d7397ea9617f05e5d7eae5c4506c0685b36
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-01 09:14:24 -0700

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+/*
+ * Auth: Jhonier Santana
+ */
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memomry
+ * @str: pointer
+ *
+ * Return: pointer to the duplicated string, in returns NULL if insufficient meomry was available
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i, j;
+	char *s;
+
+	if (str = NULL)
+	{
+		return (NULL);
+	}
+	
+	for (i = 0; i < str[i]; i++);
+	s = malloc(i * sizeof(char));
+	
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < i; i++)
+	{
+		s[j] = str[j];
+	}		
+	return (s);
+}
