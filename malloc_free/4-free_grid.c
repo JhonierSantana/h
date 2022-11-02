3e5fc2d9e369e133ca501195a112b9e2dd29141b
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-02 11:20:43 -0700

    4-free_grid.c

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -4,9 +4,9 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * free_grid - frees a 2 dimensional grid previously
- * @grid: bidimimensional pointer
- * @heigth: argument
+ * free_grid - function
+ * @height: length
+ * @grid: a bidimensional pointer
  */
 void free_grid(int **grid, int height)
 {
