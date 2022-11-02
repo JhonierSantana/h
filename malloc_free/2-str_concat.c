488b23ca5cf8cb633421199713fcce8a0ac01d46
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-02 08:53:05 -0700

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,27 +16,29 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 
 	if (s1 == NULL)
-		return (NULL);
+		s1 = "";
+
 	for (a = 0; s1[a] != '\0'; a++)
 	{
 	}
+
 	if (s2 == NULL)
+		s2 = "";
+
+	for (b = 0; s2[b] != '\0'; b++)
 	{
-		return (NULL);
 	}
-	for (b = 0; s2[b]; b++)
-	{
-	}
+
 	str = malloc(sizeof(char) * a + b + 1);
 
 	if (str == NULL)
-	{
 		return (NULL);
-	}
+
 	for (c = 0; s1[c] != '\0'; c++)
 		str[c] = s1[c];
+
 	for (d = 0; s2[d] != '\0'; d++)
-		str[d + a] = s2[d];
-		
+		str[d] = s2[d];
+
 	return (str);
 }
