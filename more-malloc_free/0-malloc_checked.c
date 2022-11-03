fe8b2200a891b2b7f8bf7efb1fffa27a04aecf7c
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-03 08:34:14 -0700

    main.h

---
+++ more-malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+/*
+ * Auth: Jhonier Santana
+ */
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - that allocates memory
+ * @b: pointer 
+ *
+ * Return: 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *a;
+
+	a = malloc(sizeof(int) * b);
+	return (a);
+}
