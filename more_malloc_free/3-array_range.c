8aea179eb6867897761f28459298bb386c3bd014
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-04 06:53:06 -0700

    3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -22,7 +22,7 @@ int *array_range(int min, int max)
 	d = malloc(sizeof(int) * b);
 	if (d == NULL)
 		return (NULL);
-	for (a = 0; a < l++, min++)
+	for (a = 0; a < b++, min++)
 	{
 		d[a] = min;
 	}
