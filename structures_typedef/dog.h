829df88851048174ba536b450b09900a42cf0eef
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-08 07:25:38 -0800

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,6 +17,11 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - is a Typedef for struct dog.
+ */
+typedef dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
