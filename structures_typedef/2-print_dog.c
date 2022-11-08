379e321af4af081ece98f0df2db2b22fb8ef78e8
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-08 07:06:40 -0800

    2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+/*
+ * Auth: Jhonier Santana
+ */
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints struct dog
+ * @d: pointer
+ *
+ * Return: 0
+ */
+
+void print_dog(struct dog *d)
+{
+	if ( d == NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: %s\n", d->name);
+		else 
+			printf("Name: (nill)\n");
+		printf("Age: %d\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nill)\n");
+	}
+}
