a7505b9163c55ad553cbe1ceaeea27427c756262
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-11 08:43:46 -0800

    1-print_numbers.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+/*
+ * Auth: Jhonier Santana
+ */
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers, followed by a new line.
+ * @separator: separator to print between numbers
+ * @n: number of numbers to print
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int num;
+	va_list ap;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(ap, int);
+		printf("%d", num);
+	}
+	printf('\n');
+	va_end(ap);
+}
