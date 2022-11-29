f83aae74e905c9b67f19ef7e34972f50b302075d
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-29 10:37:09 -0500

    .\1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: pointer to list to print.
+ * 
+ * Return: 0
+*/
+size_t list_len(const list_t *h);
+{
+    size_t a = 0;
+
+    while(h != NULL)
+    {
+        a += 1;
+        h = h->next;
+    }
+    return (a);
+}
