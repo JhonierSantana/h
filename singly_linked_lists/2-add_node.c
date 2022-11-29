46fda7afacbad8544a02a26a9dfc93bddda80762
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-29 11:14:45 -0500

    .\1-list_len.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -15,7 +15,9 @@ list_t *add_node(list_t **head, const char *str)
     
     a = (list_t *)malloc(sizeof(list_t));
     if (a == NULL)
+    {
         return (NULL);
+    }
 	len = strlen(str);
 	dup = strdup(str);
 	if (dup == NULL)
