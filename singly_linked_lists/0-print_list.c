10bbabdae9816495f3b108ed72ab2c1fb450d412
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-29 09:41:57 -0500

    .\0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,1 +1,24 @@
 #include "lists.h"
+
+/**
+ * print_list - prints all the elements
+ * @h: pointer to list to print
+ * return: 0
+ */
+
+size_t print_list(const list_t *h)
+{
+    size_t a = 0;
+
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+        {
+            printf("[0] (nill)\n");
+            a += 1;
+        }
+        printf("%i %s\n", h->len, h->str);
+        a += 1;
+    }
+    return (a);
+}
