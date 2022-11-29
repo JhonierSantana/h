0a42e8e72ec97d6ee232bd2d0f05a9060a5dcc3e
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-29 15:47:07 -0500

    arreglo

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,13 +8,16 @@ size_t print_list(const list_t *h)
 {
 size_t a = 0;
 while (h != NULL)
+
 {
 if (h->str == NULL)
 {
 printf("[0] (nil)\n");
 h = h->next;
 a += 1;
+
 }
+
 printf("[%i] %s\n", h->len, h->str);
 a += 1;
 h = h->next;
