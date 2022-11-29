2fdc0b88e650cfb79d054f642ab4816267503d6f
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-29 09:46:46 -0500

    .\0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,10 +15,12 @@ size_t print_list(const list_t *h)
         if (h->str == NULL)
         {
             printf("[0] (nill)\n");
+            h = h->next;
             a += 1;
         }
-        printf("%i %s\n", h->len, h->str);
+        printf("[%i] %s\n", h->len, h->str);
         a += 1;
+        h = h->next;
     }
     return (a);
 }
