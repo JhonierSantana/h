38bac0f51394b95b2ac7f94b8c962ee74aff6b96
JhonierSantana <Jhonier_2504@hotmail.com>
2022-11-29 11:04:43 -0500

    .\1-list_len.c

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: pointer 
+ * @str: String 
+ * Return: 0
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *dup;
+	int len;
+	list_t *a;
+
+	if (a == NULL)
+		return (NULL);
+	len = strlen(str);
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+	a->str = dup;
+	a->len = len;
+	a->next = *head;
+	return (ret);
+}
