f912f5cc4760ad6ab1ece67f24c7f1cdb1c858e8
JhonierSantana <Jhonier_2504@hotmail.com>
2022-12-01 10:04:18 -0500

    .\1-listint_len.c

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * listint_len -  returns the number of elements in a linked listint_t list.
+ * @h: header
+ * 
+ * Return: number of nodo
+*/
+size_t listint_len(const listint_t *h)
+{
+    unsigned int list_size = 0;
+
+	if (!h)
+		return (list_size);
+
+	while (h)
+	{
+		list_size += 1;
+		h = h->next;
+	}
+	return (list_size);
+}
