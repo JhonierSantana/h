746ec6c45a73a0f6b6c62328b11f5a00a490668c
JhonierSantana <Jhonier_2504@hotmail.com>
2022-12-01 10:27:54 -0500

    .\2-add_nodeint.c

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -7,23 +7,14 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node;
-	new_node = (listint_t *)malloc(sizeof(listint_t));
-	if (!new_node)
+
+	listint_t *a;
+
+	a = (listint_t *)malloc(sizeof(listint_t));
+	if (a == NULL)
 		return (NULL);
-
-	new_node->n = n;
-	new_node->next = NULL;
-
-	if (!head)
-		head = new_node;
-
-	if (!(*head))
-		*head = new_node;
-
-	else
-		new_node->next = *head;
-	*head = new_node;
-
-	return (new_node);
+	a->n = n;
+	a->next = *head;
+	*head = a;
+	return (a);
 }
