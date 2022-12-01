b4b4c2395562dcf22624330299c25ba3042927c5
JhonierSantana <Jhonier_2504@hotmail.com>
2022-12-01 09:47:23 -0500

    arreglo

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: header node
+ *
+ * Return: number of nodos
+ */
+
+size_t print_listint(const listint_t *h);
+{
+	unsigned int list_size = 0;
+
+	if (!h)
+		return list_size;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		list_size += 1;
+		h = h->next;
+	}
+	return(list_size);
+}
