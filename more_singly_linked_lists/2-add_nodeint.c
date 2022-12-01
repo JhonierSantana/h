653b1c52794bcb2643cdc89d2000289286807293
JhonierSantana <Jhonier_2504@hotmail.com>
2022-12-01 10:31:05 -0500

    .\2-add_nodeint.c

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list.
- * @h: header of nodo
- *
- * Return: adds a new node at the beginning of a listint_t list.
+ * @head: Pointer to head pointer of listint
+ * @n: intiger to add
+ * Return: Pointer to new node
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
