a0b61f05021f3be7c05a051f487fcf54b81bedba
JhonierSantana <Jhonier_2504@hotmail.com>
2022-12-05 10:52:43 -0500

    .\0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,13 +2,30 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    if (filename == NULL)
-        return (0); 
+	int fd, r;
+	char *buff;
+	if (filename == NULL)
+		return (0);
 
-    size_t a = letters;
+	buff = (char *)malloc(sizeof(char)) * letters) + 1);
+	if (buff == NULL)
+		return (0);
+	fd = open(*filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buff);
+		return (0);
+	}
 
-    int fd = open(*filename, O_RDONLY);
-    printf("%d\n %s", fd, a);
+	r = read(fd, buff, letters);
 
-    return (fd);
+	if (r == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	free(buff);
+	return (r);
 }
