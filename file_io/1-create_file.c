c20eb51c8a75dcef788d78d87a584dc3f32218c8
JhonierSantana <Jhonier_2504@hotmail.com>
2022-12-06 10:40:58 -0500

    .\1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,9 +15,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDwR, O_TRUNC);
+	fd = open(filename, O_RDWR, O_TRUNC);
 	if (fd == -1)
-		fd = open(filename, O_CREAT, O_RDwR, O_TRUNC);
+		fd = open(filename, O_CREAT, O_RDWR, O_TRUNC);
 
 	if (text_content)
 	{
