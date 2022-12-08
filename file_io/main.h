aaea7179435ab23633d5fc371e00e7c3eac19e72
JhonierSantana <Jhonier_2504@hotmail.com>
2022-12-08 09:51:56 -0500

    .\main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,18 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int main(int argc, char **argv);
-int copyfile(const char *file_from, const char *file_to);
-int closefree(int fd1, int fd2, char *buff);
 
 #endif
