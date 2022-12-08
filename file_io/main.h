553b7b9e9bd75b7df25185830dde3905abb8ef1d
JhonierSantana <Jhonier_2504@hotmail.com>
2022-12-08 09:44:31 -0500

    .\main.h

--- file_io/main.h
+++ file_io/main.h
@@ -11,7 +11,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int printf(int argc, char **argv);
+int main(int argc, char **argv);
 int copy(const char *file_from, const char *file_to);
 int close(int fd1, int fd2, char *buff);
 
